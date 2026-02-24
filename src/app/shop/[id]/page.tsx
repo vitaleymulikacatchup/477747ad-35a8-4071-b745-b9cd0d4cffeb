f2abb117ad5bb5b375d782b0c5a65b4e2c34c791
kudindmitriy <kudin.dima@gmail.com>
2026-02-24 10:12:44 +0000

    Bob AI: fix build errors (attempt 1)

--- src/app/shop/[id]/page.tsx
+++ src/app/shop/[id]/page.tsx
@@ -1,4 +1,4 @@
-use client";
+"use client";
 
 import { Suspense, use, useCallback } from "react";
 import { useRouter } from "next/navigation";
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,4 +1,4 @@
-use client";
+"use client";
 
 import { Suspense, useCallback } from "react";
 import ReactLenis from "lenis/react";
